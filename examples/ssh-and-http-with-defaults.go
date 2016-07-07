80498bcdfab66ca7e240515eb86f7e036686d0bc
Bryan Burke <bryan.t.burke@gmail.com>
2016-07-07 17:44:08 -0600

    update example for standard logging

--- examples/ssh-and-http-with-defaults.go
+++ examples/ssh-and-http-with-defaults.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"github.com/BTBurke/gitkit"
-	//kitlog "github.com/go-kit/kit/log"
 	"log"
 	"os"
 )
 
 func main() {
-	//logger := kitlog.NewJSONLogger(os.Stdout)
-	//log.SetOutput(kitlog.NewStdlibAdapter(logger))
 	log.New(os.Stdout, "", 0)
 	server := gitkit.New()
 	server.Run()
