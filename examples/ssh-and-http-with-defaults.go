ccc50f815fb97f8fed12f1135d7c2cd79521b1d8
Bryan Burke <bryan.t.burke@gmail.com>
2016-07-07 17:47:43 -0600

    update basic example

--- examples/ssh-and-http-with-defaults.go
+++ examples/ssh-and-http-with-defaults.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/BTBurke/gitkit"
 	"log"
 	"os"
 )
 
 func main() {
+	fmt.Println("This example shows a basic dual HTTP and SSH server running on ports 8080 and 2222 respectively.\n\n**Warning** Don't use this model in production as nothing is secured.  You should look at the other examples for how to configure authorization functions, TLS, and other security measures.\n")
 	log.New(os.Stdout, "", 0)
 	server := gitkit.New()
 	server.Run()
