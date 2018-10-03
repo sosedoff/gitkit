19b578c5501b604760f16d09dad45c0a6a8d3e40
Dan Sosedoff <dan.sosedoff@gmail.com>
2018-10-03 17:10:59 -0500

    Refactor/rename stuff

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 test:
-	godep go test -v -race -cover .
+	go test -v -race -cover .
 
 build:
-	godep go build
+	go build
 
 all:
 	gox -osarch="darwin/amd64 linux/amd64" -output="gitkit_{{.OS}}_{{.Arch}}"
--- credential_test.go
+++ credential_test.go
@@ -7,15 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_getAuth(t *testing.T) {
+func Test_getCredential(t *testing.T) {
 	req, _ := http.NewRequest("get", "http://localhost", nil)
-	_, err := getAuth(req)
+	_, err := getCredential(req)
 	assert.Error(t, err)
 	assert.Equal(t, "authentication failed", err.Error())
 
 	req, _ = http.NewRequest("get", "http://localhost", nil)
 	req.SetBasicAuth("Alladin", "OpenSesame")
-	cred, err := getAuth(req)
+	cred, err := getCredential(req)
 
 	assert.NoError(t, err)
 	assert.Equal(t, "Alladin", cred.Username)
