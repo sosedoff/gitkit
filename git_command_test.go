0684d5b81752b62c64f7f058736cf7be373e4ce6
Dan Sosedoff <dan.sosedoff@gmail.com>
2018-10-05 14:07:06 -0500

    Remove leading slash from reponame in ssh handler

--- git_command_test.go
+++ git_command_test.go
@@ -10,6 +10,7 @@ func TestParseGitCommand(t *testing.T) {
 	examples := map[string]GitCommand{
 		"git-upload-pack 'hello.git'":    GitCommand{"git-upload-pack", "hello.git", "git-upload-pack 'hello.git'"},
 		"git upload-pack 'hello.git'":    GitCommand{"git upload-pack", "hello.git", "git upload-pack 'hello.git'"},
+		"git-upload-pack '/hello.git'":   GitCommand{"git-upload-pack", "hello.git", "git-upload-pack 'hello.git'"},
 		"git-receive-pack 'hello.git'":   GitCommand{"git-receive-pack", "hello.git", "git-receive-pack 'hello.git'"},
 		"git receive-pack 'hello.git'":   GitCommand{"git receive-pack", "hello.git", "git receive-pack 'hello.git'"},
 		"git-upload-archive 'hello.git'": GitCommand{"git-upload-archive", "hello.git", "git-upload-archive 'hello.git'"},
